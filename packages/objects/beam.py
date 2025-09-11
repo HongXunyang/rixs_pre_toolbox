f026e92c9010b803bd08d904d317909987fa0f49
HongXunyang <519109830@qq.com>
2025-09-11 13:13:22 +0200

    clean up

--- packages/objects/beam.py
+++
@@ -1,9 +0,0 @@
-"""This is the beam class for the beam object."""
-
-
-class Beam:
-    """This is the beam class for the beam object."""
-
-    def __init__(self):
-        """Initialize the beam object."""
-        pass
